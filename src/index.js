feb3095bfef2521adf3641f5d16f7bff7a4f6f00
ruchir saxena <ruchirsaxena@gmail.com>
2018-06-27 00:42:08 +0530

    Using Classes in ES6 -Basic Usage

--- src/index.js
+++ src/index.js
@@ -1,5 +1,31 @@
-function greet(name="david") {
-    return "hello "+name;
+class Person{
+   constructor(name ,profession){
+       this.name=name;
+       this.profession = profession;
+   }
+
+   getPersonInfo(){
+       alert(`Person name is :${this.name} and his profession is :${this.profession}`);
+   }
+
 }
 
-console.log(greet());
+class Coder extends Person{
+    constructor(name,profession,tecnologyStack){
+        super(name, profession);
+        this.tecnologyStack=tecnologyStack;
+    }
+
+    getInfo(){
+        alert(`Technology Stack :${this.tecnologyStack}`);
+    }
+}
+
+var personObj=new Person("Ruchir","coder");
+
+//personObj.getPersonInfo();
+var coderObj=new Coder("Ruchir","coder","Javascript");
+
+coderObj.getPersonInfo();
+coderObj.getInfo();
+
